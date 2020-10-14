c33d55343d2ad7c5505814ca63092eb7e33f0fcf
Joseph <Josephknelson@lewisu.edu>
2020-10-14 12:09:18 -0500

    initial commit

---
+++ meadowlark.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.get('/about', (req, res) => {
+    res.render('about', { fortune: fortune.getFortune() })
+})
+
+
+app.get('/', (req, res) => res.render('home'))
+
+
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
